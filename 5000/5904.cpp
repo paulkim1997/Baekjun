3895d7b96bfe65b95f64bd228c005cea5b29e71a
paulkimtest <48825944+paulkim1997@users.noreply.github.com>
2021-04-30 23:43:22 +0900

    Update 5904.cpp

--- 5000/5904.cpp
+++ 5000/5904.cpp
@@ -10,9 +10,12 @@ int main() {
     cin >> n;
 
     //입력한 n 이 몇 번째 수열에 포함되는지?
-    //totLength = 수열 전체 길이, midLength = 수열의 중간 부분 길이
+    //totLength = 수열 전체 길이, midLength = o가 k+2개인 부분 길이
+    //초기 상태 : moo
     int totLength = 3;
     int midLength = 3;
+
+    //n이 현재 수열의 길이보다 크면 수열 계속 늘려주기
     while(n > totLength) {
         totLength = totLength*2 + ++midLength;
     }
@@ -21,14 +24,21 @@ int main() {
         //t 는 이전 문자열의 길이
         int t = (totLength-midLength)/2;
 
+        //n이 첫 번째 부분이면
+        //o가 k+2개인 부분의 길이를 1 줄여주고, 전체 길이를 이전 문자열의 길이로
         if(n <= t) {
             midLength--;
             totLength = t;
-        } else if(n > t + midLength) {
+        }//n이 세 번째 부분이면
+            //이전 문자열의 길이 만큼 줄여주며 사라진 만큼 n도 줄여준다
+            //o가 k+2개인 부분의 길이를 1 줄여주고, 전체 길이를 이전 문자열의 길이로
+        else if(n > t + midLength) {
             n -= t + midLength;
             midLength--;
             totLength = t;
-        } else {
+        }//n이 중간 부분이면
+            //n에서 이전 문자열의 크기만 빼준다.
+        else {
             n -= t;
             break;
         }
